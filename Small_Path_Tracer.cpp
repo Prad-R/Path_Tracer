1a125448ca78f7d24c34fe60a76878abe7cdbf00
Prad-R <pradl12121@gmail.com>
2024-04-10 20:18:03 +0530

    Wrote radiance

--- Small_Path_Tracer.cpp
+++ Small_Path_Tracer.cpp
@@ -34,7 +34,7 @@ struct Vec{
         return Vec(x * b.x, y * b.y, z * b.z);
     }
 
-    // Norm
+    // Unit vector or normalized vector
     Vec& norm(){
         return *this = *this * (1 / sqrt(x * x + y * y + z * z));
     }
@@ -101,7 +101,9 @@ Sphere spheres[] = {// Array of spheres to set the scene
 };
 
 inline double clamp(double x) {return x < 0 ? 0 : x > 1 ? 1 : x;} // Function essentially bounds values into [0,1]
+
 inline int toInt(double x) {return int(pow(clamp(x), 1/2.2) * 255 + 0.5);} // 2.2 is gamma correction factor, 255 is used to scale the color value in [0,256]
+
 inline bool intersect(const Ray &r, double &t, int &id){
     double n = sizeof(spheres) / sizeof(Sphere), d, inf = t = 1e20; // number of spheres, distance and maximum distance.
     
@@ -112,4 +114,69 @@ inline bool intersect(const Ray &r, double &t, int &id){
         }
     }
     return t < inf; // Returns bool
+}
+
+// This function returns the color of a pixel corresponding to a ray.
+Vec radiance(const Ray &r, int depth, unsigned short *Xi){ // *Xi is the pointer to an array of possible seeds.
+    double t; // Distance to intersection
+    int id = 0; // ID of intersected object
+
+    // Returning black color if no intersection
+
+    if (!intersect(r, t, id))
+        return Vec();
+
+    const Sphere &obj = spheres[id]; // The object hit by the ray
+    Vec x = r.o + r.d * t; // The intersection point
+    Vec n = (x - obj.p).norm(); // Normal vector at the intersection
+    Vec n1 = n.dot(r.d) < 0 ? n : n * -1; // Ensuring the normal vector makes outwards from the object
+    Vec f = obj.c; // Surface color of the object
+    double p = f.x > f.y && f.x > f.z ? f.x : f.y > f.z ? f.y : f.z; // Max reflection coefficient
+
+    // Russian Roulette : randomly terminating rays to avoid infinite recursion
+    if (++depth > 5) // If depth of recursion (number of reflections or refractions) exceeds a number, terminate the ray early
+        if (erand48(Xi) < p)
+            f = f * (1 / p); // If the randomg number is < p, the ray's intensity is scaled 1/p to conserve energy by compensating for early termination.
+        else
+            return obj.e;
+
+    // Ideal diffuse reflection
+    if (obj.refl == DIFF){
+        double r1 = 2 * M_PI * erand48(Xi); // Generates a random angle in [0, 2pi]
+        double r2 = erand48(Xi); // cosine-weighted distribution of driections used to simulate more likely reflections along direction closer to normal.
+        double r2s = sqrt(r2); // Square root of r2
+
+        Vec w = n1; // Corrected surface normal
+        Vec u = (fabs(w.x) > 0.1 ? Vec(0, 1) : Vec (1)).%w.norm(); // u is a 2D tangent vector to n1. The condition checks if w is parallel to the y-axis.
+        Vec v = w.%u; // Bitangent vector perpendicular to both u and w
+        Vec d = (u * cos(r1) * r2s + v * sin(r1) * r2s + w * sqrt(1 - r2)).norm(); // Random reflected direction following a cosine-weighted distribution
+        // Note that the direction of the reflected ray is basically a linear combination of u, v and w.
+
+        return obj.e + f.mult(radiance(Ray(x, d), depth, Xi));
+    }
+
+    // Ideal specular reflection
+    else if (obj.refl == SPEC){
+        return obj.e + f.mult(radiance(Ray(x, r.d - n1 * 2 * n1.dot(r.d)), depth, Xi));
+    }
+
+    // Ideal Dielectric Refraction
+    Ray reflRay(x, r.d - n1 * 2 * n1.dot(r.d));
+    bool into = n.dot(n1)> 0; // Checking if the normal and corrected normal are pointing in the same way
+    double nc = 1, nt = 1.5, nnt = into ? nc / nt : nt / nc, ddn = r.d.dot(n1), cos2t;
+    // nc is the mu of medium 1. nt is the mu of medium 2. nnt is the ratio of refractive indices depending on whether the light is going in or out.
+    // ddn is the cosine of the angle between the ray and the correct normal
+    
+    if ((cos2t = 1 - nnt * nnt * (1 - ddn * ddn)) < 0) // Checks is the angle of refraction is obtuse. If so TIR occurs
+        return obj.e + f.mult(radiance(reflRay, depth, Xi)); // In case of TIR, only the reflected component of the ray is considered
+
+    Vec tdir = (r.d * nnt - n * ((into ? 1 : -1) * (ddn * nnt  + sqrt(cos2t)))).norm(); // Direction of refracted ray
+
+    double a = nt - nc, b =  nt + nc, RO = a * a / (b * b), c = 1 - (into ? - ddn : tdir.dot(n));
+    double Re = RO + (1 - RO) * c * c * c * c * c, Tr = 1 - Re, P = 0.25 + 0.5 * Re, RP = Re / P, TP = Tr / (1 - P); 
+    // Fresnel equation to determine coefficients of reflection and transmission
+    // RP and TP are reflection and transmission probabilities
+    return obj.e + f.mult(depth > 2 ? (erand48(Xi) < P ? radiance(reflRay, depth, Xi) * RP : radiance(Ray(x, tdir), depth, Xi) * TP) : radiance(reflRay, depth, Xi) * Re + radiance(Ray(x, tdir), depth, Xi) + Tr); 
+    // For more than two recursions, the RP and TP are used to scale the next ray's contribution.
+    // For less than two recursions, the new ray is simply a linear combination of the reflected and transmitted rays.
 }
